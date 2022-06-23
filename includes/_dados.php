3f03e33a63dc1ee1b88e37672f7e7621a86a2033
ThizinhoSRS <93769448+ThizinhoSRS@users.noreply.github.com>
2022-06-23 12:23:29 -0300

    qualquer coisa

--- includes/_dados.php
+++ includes/_dados.php
@@ -1,12 +1,12 @@
 <?php
 
 $produtos = array(
-    0 => array('nome'=>'gustavo','preco'=>'4.000','descricao'=>'gustavo','imagem'=>'gustavo.jpg'),
-    1 => array('nome'=>'thiago','preco'=>'4.000','descricao'=>'gustavo','imagem'=>'doberman.jpg'),
-    2 => array('nome'=>'samuel','preco'=>'4.000','descricao'=>'gustavo','imagem'=>'frances.jpg'),
-    3 => array('nome'=>'lucas','preco'=>'4.000','descricao'=>'gustavo','imagem'=>'ingles.jpg'),
-    4 => array('nome'=>'anavoto','preco'=>'4.000','descricao'=>'gustavo','imagem'=>'lulu.jpg'),
-    5 => array('nome'=>'alice','preco'=>'4.000','descricao'=>'gustavo','imagem'=>'gustavo.jpg'),
+    0 => array('nome'=>'Gustavo','preco'=>'4.000','descricao'=>'Gustavo feio','imagem'=>'gustavo.jpg'),
+    1 => array('nome'=>'Thiago','preco'=>'4.000','descricao'=>'Thiago ultra macho','imagem'=>'doberman.jpg'),
+    2 => array('nome'=>'Samuel','preco'=>'4.000','descricao'=>'Samuel ultra fofo','imagem'=>'frances.jpg'),
+    3 => array('nome'=>'Lucas','preco'=>'4.000','descricao'=>'Lucas gordão','imagem'=>'ingles.jpg'),
+    4 => array('nome'=>'Anavoto','preco'=>'4.000','descricao'=>'Botei o nome aleatorio','imagem'=>'lulu.jpg'),
+    5 => array('nome'=>'Alice','preco'=>'4.000','descricao'=>'PQ ALICE?','imagem'=>'gustavo.jpg'),
     6 => array('nome'=>'julia','preco'=>'4.000','descricao'=>'gustavo','imagem'=>'gustavo.jpg'),
     7 => array('nome'=>'pedro','preco'=>'4.000','descricao'=>'gustavo','imagem'=>'gustavo.jpg'),
     8 => array('nome'=>'gabriela','preco'=>'4.000','descricao'=>'gustavo','imagem'=>'gustavo.jpg'),
--- produto-detalhe.php
+++ produto-detalhe.php
@@ -3,9 +3,24 @@
 include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
+$id = $_GET['id'];
+$tipo = $_GET['tipo'];
 ?>
-
-<h1>Produto Detalhe</h1>
+<div class="container">
+    <div class="row">
+        <div class="col">
+    <h1>Produto Detalhe</h1>
+</div>
+            <div id="detalhe" class="container">
+                <div id="produto-detalhe">
+            <h1><?php echo $produtos[$id]['nome'];?>
+            <img id="img-animal" src="./content/<?php echo $produtos[$id]['imagem'];?>" class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card-text"><?php echo $produtos[$id]['descricao'];?></p>
+            <h4>R$ <?php echo $produtos[$id]['preco'];?></h4></div>
+        </div>
+    </div>
+</div>
 
 <?php
 // include do footer
